ef8ad3bdc7f0bb935b026219c3c15d944c3725e6
Kariuki_Ken <irungukenneth433@gmail.com>
2024-07-27 21:06:43 +0300

    New changes

---
+++ api/views.py
@@ -0,0 +1,15 @@
+import requests
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+from django.conf import settings
+weather_apikey = settings.WEATHER_API_KEY
+BASE_URL = 'http://api.openweathermap.org/data/2.5/weather'
+
+class WeatherView(APIView):
+    def get(self, request, *args, **kwargs):
+        city = kwargs.get(city)
+        url = f"{BASE_URL}?q={city}&appid={weather_apikey}&units=metric"
+        weather = requests.get(url)
+                
