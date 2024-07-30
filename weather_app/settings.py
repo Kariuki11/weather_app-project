354d13e44a7e7b3b71c4e18fc25157e3ceee6d43
Kariuki_Ken <irungukenneth433@gmail.com>
2024-07-30 20:42:56 +0300

    New Requirements.txt

--- weather_app/settings.py
+++ weather_app/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGIN=['https://weathertrackapp.onrender.com']
 
 
 # Application definition
